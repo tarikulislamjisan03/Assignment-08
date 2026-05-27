6926389cf5793dc5cd310a5d4e2557efca297e67
Tarikul Islam Jisan <tarikulislamjisan03@gmail.com>
2026-05-27 11:17:46 +0600

    learnign tips

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/Banner";
+import LearningTips from "@/components/LearningTips";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex container mx-auto flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
      <Banner> </Banner>
+      <LearningTips> </LearningTips>
     </div>
   );
 }
