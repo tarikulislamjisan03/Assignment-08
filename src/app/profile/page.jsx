3eeab9e8decad93e234f1e1ef0bf9f938747a738
Tarikul Islam Jisan <tarikulislamjisan03@gmail.com>
2026-06-02 07:05:16 +0600

    proxy protect

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,9 +1,31 @@
+"use client"
+
+import { ProfileModal } from '@/components/ProfileModal';
+import { authClient } from '@/lib/authclient';
+import { Avatar, Card } from '@heroui/react';
 import React from 'react';
 
 const ProfilePage = () => {
+     const userdata = authClient.useSession();
+      const user = userdata.data?.user;
+      console.log(user)
     return (
         <div>
-            <h1 className='text-4xl text-red-500 ' >hello from profiel</h1>
+            <Card className='max-w-96 h-52 mx-auto flex flex-col items-center justify-center shadow-lg mt-20 mb-20'> 
+                 <Avatar className="w-12 h-12" size='5'>
+                                <Avatar.Image 
+                                  alt={user?.name || "User"} 
+                                  src={user?.image} 
+                                  referrerPolicy="no-referrer"  
+                                />
+                                <Avatar.Fallback>
+                                  {user?.name ? user.name[0].toUpperCase() : "U"}
+                                </Avatar.Fallback>
+                              </Avatar>
+                              <h1> {user?.name} </h1>
+                               <h1> {user?.email} </h1>
+                              <ProfileModal> </ProfileModal>
+            </Card>
         </div>
     );
 };
---
+++ src/proxy.js
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server'
+import { auth } from './lib/auth'
+import { headers } from 'next/headers'
+ 
+// This function can be marked `async` if using `await` inside
+export async function proxy(request) {
+
+
+    const session=await auth.api.getSession({
+        headers:await headers()
+    })
+
+    if(!session){
+         return NextResponse.redirect(new URL('/register', request.url))
+    }
+ 
+}
+ 
+export const config = {
+  matcher: ['/profile','/courses','/courses/:path*'],
+}
