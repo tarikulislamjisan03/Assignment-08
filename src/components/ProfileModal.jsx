4a2fed7d5d322eeaf555549ab8fb88b64113d0c9
Tarikul Islam Jisan <tarikulislamjisan03@gmail.com>
2026-06-02 23:55:15 +0600

    updataeuser

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -1,9 +1,21 @@
 "use client";
 
+import { authClient } from "@/lib/authclient";
 import {Envelope} from "@gravity-ui/icons";
 import {Button, Input, Label, Modal, Surface, TextField} from "@heroui/react";
 
 export function ProfileModal() {
+
+  const handlemodal=async(e)=>{
+e.preventDefault()
+const name=e.target.name.value
+const image=e.target.image.value
+console.log(name,image)
+await authClient.updateUser({
+  name,
+  image
+})
+  }
   return (
     <Modal>
       <Button variant="secondary">Update Profile</Button>
@@ -19,25 +31,26 @@ export function ProfileModal() {
             </Modal.Header>
             <Modal.Body className="p-6">
               <Surface variant="default">
-                <form className="flex flex-col gap-4">
+                <form onSubmit={handlemodal} className="flex flex-col gap-4">
                   <TextField className="w-full" name="name" type="text" variant="secondary">
                     <Label>Name</Label>
                     <Input placeholder="Enter your name" />
                   </TextField>
-                  <TextField className="w-full" name="text" type="text" variant="secondary">
+                  <TextField className="w-full" name="image" type="text" variant="secondary">
                     <Label>Image URL</Label>
                     <Input placeholder="Url" />
                   </TextField>
+                   <Modal.Footer>
+              <Button slot="close" variant="secondary">
+                Cancel
+              </Button>
+              <Button type="submit" slot="close">Update</Button>
+            </Modal.Footer>
             
                 </form>
               </Surface>
             </Modal.Body>
-            <Modal.Footer>
-              <Button slot="close" variant="secondary">
-                Cancel
-              </Button>
-              <Button slot="close">Update</Button>
-            </Modal.Footer>
+           
           </Modal.Dialog>
         </Modal.Container>
       </Modal.Backdrop>
